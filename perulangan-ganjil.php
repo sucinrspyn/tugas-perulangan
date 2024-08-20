ca3d418c76582c0379c81b45a4882fac3f2a2573
suci <sopyansuci042@gmail.com>
2024-08-20 14:00:19 +0700

    first commit

---
+++ perulangan-ganjil.php
@@ -0,0 +1,7 @@
+<?php
+echo "Bilangan ganjil antara 1 sampai 100:<br>";
+$i = 1;
+while ($i <= 100) {
+    echo $i . "<br>";
+    $i += 2;
+}
---
+++ perulangan-genap.php
@@ -0,0 +1,7 @@
+<?php
+echo "Bilangan genap antara 1 sampai 100:<br>";
+$i = 2;
+while ($i <= 100) {
+    echo $i . "<br>";
+    $i += 2;
+}
